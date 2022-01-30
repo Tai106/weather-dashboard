d0c541791338dc539b625571109d3ef51290fcd0
Gravity <tevoned@gmail.com>
2022-01-30 01:42:55 -0500

    header

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let apiKey = "89a69d56167ac6a162182ae97ce41904";
+let searchBtn = $(".searchBtn");
+let searchInput = $(".searchInput");
+
+let cityName = $(".cityName");
+let weatherIcon = $(".weatherIcon");
+let searchHistory = $(".historyItems");
+let currentDate = $(".currentDate");
